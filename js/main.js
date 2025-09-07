f30d17052d27a7a801b88f115ddb8129cb914c7f
IgorPortyanko <igor.portyanko82@gmail.com>
2025-09-07 12:52:18 +0300

    change main.js

--- js/main.js
+++ js/main.js
@@ -2,17 +2,22 @@
 
 const taskList = document.querySelector('.menu');
 const content = document.querySelector('.content');
+render('./pages/task-1.html')
 
 taskList.addEventListener('click', function (event) {
+
     const link = event.target.closest('a');
+
     if (link) {
         event.preventDefault();
         const url = link.getAttribute('href');
+        render(url)
+    }
+})
 
+function render(url) {
         if (!url) return;
-
         content.innerHTML = '<p>Завантаження...</p>';
-
 
         fetch(url)
             .then(response => {
@@ -23,26 +28,26 @@ taskList.addEventListener('click', function (event) {
             })
             .then(html => {
                 content.innerHTML = html;
-                const existingScripts = document.querySelectorAll('script[data-dynamic]');
-                existingScripts.forEach(script => script.remove());
-
-                const scripts = content.querySelectorAll('script');
-
-                scripts.forEach(script => {
-
-                    const newScript = document.createElement('script');
-                    newScript.src = script.src;
-                    newScript.setAttribute('data-dynamic', 'true');
-
-                    newScript.onload = () => console.log(`Скрипт завантажено: ${script.src}`);
-                    newScript.onerror = () => console.error(`Помилка завантаження скрипта: ${script.src}`);
-
-                    document.body.appendChild(newScript);
-
-                });
+                createScript()
             })
             .catch(error => {
                 content.innerHTML = `<p style="color:red;">${error.message}</p>`;
             });
-    }
-})
+}
+
+function createScript() {
+    const existingScripts = document.querySelectorAll('script[data-dynamic]');
+
+    existingScripts.forEach(script => script.remove());
+
+    const scripts = content.querySelectorAll('script');
+
+    scripts.forEach(script => {
+
+        const newScript = document.createElement('script');
+        newScript.src = script.src;
+        newScript.setAttribute('data-dynamic', 'true');
+
+        document.body.appendChild(newScript);
+    });
+}
